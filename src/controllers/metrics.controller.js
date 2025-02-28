f17fecaf2251463bb6b4bec78f4786da5e77ac2d
Carlos Bohorquez <carlos.bohorquez@agilityrpa.com>
2025-02-28 16:29:57 -0500

    Add route and controller for read metrics by player.

--- src/controllers/metrics.controller.js
+++ src/controllers/metrics.controller.js
@@ -20,12 +20,34 @@ export const addMetric = async (req, res) => {
 }
 
 
-export const readMetric = async (req, res) => {
-    try {
+export const readMetricByPlayer = async (req, res) => {
+    try 
+    {
+      const { playerId } = req.params;
+      if (!playerId) {
+        return res.status(400).json({ error: "El playerId es requerido." });
+      }
+
+      const playerStatsRef = db.collection("playerStats");
+      const querySnapshot = await playerStatsRef.where("playerId", "==", playerId).get();
+  
+      if (querySnapshot.empty) {
+        return res.status(404).json({ message: "No se encontraron métricas para este jugador." });
+      }
+
+      const metrics = [];
+      querySnapshot.forEach((doc) => {
+        metrics.push({ id: doc.id, ...doc.data() });
+      });
+  
+      res.status(200).json(metrics);
+
+
+
   
     }
     catch(error){
-      console.log(error)
-          res.status(500).json({ error: "Error al crear nuevo jugador: "+ error });
+          console.log(error)
+          res.status(500).json({ error: "Error al obtener métricas: " + error.message });
     }
   }
--- src/routes/metrics.routes.js
+++ src/routes/metrics.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addMetric,readMetric } from "../controllers/metrics.controller.js";
+import { addMetric,readMetricByPlayer } from "../controllers/metrics.controller.js";
 
 const router = express.Router();
 
 router.post("/add-metric", addMetric);
-router.post("/read-metric", readMetric);
+router.get("/read-metric/:playerId", readMetricByPlayer);
 
 
 export default router;
