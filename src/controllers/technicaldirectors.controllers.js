fa0012f4bf0ea5591ea1b6c7085cfc4afc05ce1a
Carlos Bohorquez <carlos.bohorquez@agilityrpa.com>
2025-03-02 15:49:15 -0500

    Add create user when create new technical director.

--- src/controllers/technicaldirectors.controllers.js
+++ src/controllers/technicaldirectors.controllers.js
@@ -8,14 +8,29 @@ export const addTechnical = async (req, res) => {
         if (!schoolId || !directors || !Array.isArray(directors)) {
             return res.status(400).json({ error: "Datos inválidos" });
         }
-        const batch = db.batch();
+        const batch = db.batch();        
 
         directors.forEach((director) => {
             const docRef = db.collection("tecnicos").doc();
-            batch.set(docRef, { ...director, schoolId, createdAt: new Date() });
+            const { password, ...directorData } = director;
+            
+            batch.set(docRef, { ...directorData, schoolId, createdAt: new Date() });
+
+            const docUserRef = db.collection("Users").doc();
+            batch.set(docUserRef, { 
+              email:director.email,
+              name: director.firstName,
+              lastname: director.lastName,
+              role: "tecnico",
+              password: director.password,
+              schoolId, 
+              createdAt: new Date() });
+
+
+
           });      
           await batch.commit();
-          res.status(201).json({ message: "Tecnicos creadas exitosamente" });
+          res.status(201).json({ message: "Tecnicos y usuarios creadas exitosamente." });
 
 
     }
